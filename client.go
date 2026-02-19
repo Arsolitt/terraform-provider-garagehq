39b0d260e018007be0fee38c6987a23ad04a74ef
Arsolitt <arsolitt@gmail.com>
2026-02-19 20:16:07 +0300

    refactor(client)!: migrate to garage admin api v2
    - Add WithAuth helper for bearer token context
    - Store scheme/host for S3 lifecycle calls
    - Update resource_garage_key for v2 API types
    
    BREAKING CHANGE: Requires Garage v2.x server

--- client.go
+++ client.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"context"
 
 	garage "git.deuxfleurs.fr/garage-sdk/garage-admin-sdk-golang"
 )
 
 type GarageClient struct {
-	Client *garage.APIClient
+	Client  *garage.APIClient
+	Token   string
+	Scheme  string
+	Host    string
 }
 
 func NewGarageClient(scheme, host, token string) (*GarageClient, error) {
 	cfg := garage.NewConfiguration()
 	cfg.Scheme = scheme
 	cfg.Host = host
-	cfg.DefaultHeader["Authorization"] = fmt.Sprintf("Bearer %s", token)
 
 	client := garage.NewAPIClient(cfg)
-	return &GarageClient{Client: client}, nil
+	return &GarageClient{
+		Client: client,
+		Token:  token,
+		Scheme: scheme,
+		Host:   host,
+	}, nil
+}
+
+func (c *GarageClient) WithAuth(ctx context.Context) context.Context {
+	return context.WithValue(ctx, garage.ContextAccessToken, c.Token)
 }
--- resource_garage_key.go
+++ resource_garage_key.go
@@ -41,10 +41,10 @@ func resourceGarageKeyCreate(ctx context.Context, d *schema.ResourceData, m inte
 	client := m.(*GarageClient)
 	name := d.Get("name").(string)
 
-	keyBody := garage.NewUpdateKeyRequestBody()
-	keyBody.SetName(name)
+	keyReq := garage.NewUpdateKeyRequestBody()
+	keyReq.SetName(name)
 
-	key, resp, err := client.Client.AccessKeyAPI.CreateKey(ctx).Body(*keyBody).Execute()
+	key, resp, err := client.Client.AccessKeyAPI.CreateKey(client.WithAuth(ctx)).Body(*keyReq).Execute()
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("failed to create key: %w", err))
 	}
@@ -54,16 +54,13 @@ func resourceGarageKeyCreate(ctx context.Context, d *schema.ResourceData, m inte
 		}
 	}()
 
-	d.SetId(key.AccessKeyId)
-	if err := d.Set("access_key_id", key.AccessKeyId); err != nil {
+	d.SetId(key.GetAccessKeyId())
+	if err := d.Set("access_key_id", key.GetAccessKeyId()); err != nil {
 		return diag.FromErr(err)
 	}
-	if key.SecretAccessKey.IsSet() {
-		secret := key.SecretAccessKey.Get()
-		if secret != nil {
-			if err := d.Set("secret_access_key", *secret); err != nil {
-				return diag.FromErr(err)
-			}
+	if secret, ok := key.GetSecretAccessKeyOk(); ok && secret != nil {
+		if err := d.Set("secret_access_key", *secret); err != nil {
+			return diag.FromErr(err)
 		}
 	}
 
@@ -74,7 +71,7 @@ func resourceGarageKeyRead(ctx context.Context, d *schema.ResourceData, m interf
 	client := m.(*GarageClient)
 	keyID := d.Id()
 
-	key, resp, err := client.Client.AccessKeyAPI.GetKeyInfo(ctx).Id(keyID).Execute()
+	key, resp, err := client.Client.AccessKeyAPI.GetKeyInfo(client.WithAuth(ctx)).Id(keyID).Execute()
 	if err != nil {
 		if resp != nil && resp.StatusCode == http.StatusNotFound {
 			d.SetId("")
@@ -88,33 +85,53 @@ func resourceGarageKeyRead(ctx context.Context, d *schema.ResourceData, m interf
 		}
 	}()
 
-	if err := d.Set("access_key_id", key.AccessKeyId); err != nil {
+	if err := d.Set("access_key_id", key.GetAccessKeyId()); err != nil {
 		return diag.FromErr(err)
 	}
-	if err := d.Set("name", key.Name); err != nil {
+	if err := d.Set("name", key.GetName()); err != nil {
 		return diag.FromErr(err)
 	}
-	// Note: secret_access_key is not available on read, only on create
 
 	return nil
 }
 
 func resourceGarageKeyUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	// Garage doesn't support updating key names, so we recreate if name changes
+	client := m.(*GarageClient)
+	keyID := d.Id()
+
 	if d.HasChange("name") {
-		// Delete and recreate
-		diags := resourceGarageKeyDelete(ctx, d, m)
-		if diags.HasError() {
-			return diags
+		name := d.Get("name").(string)
+		updateReq := garage.NewUpdateKeyRequestBody()
+		updateReq.SetName(name)
+
+		_, resp, err := client.Client.AccessKeyAPI.UpdateKey(client.WithAuth(ctx)).Id(keyID).UpdateKeyRequestBody(*updateReq).Execute()
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to update key: %w", err))
 		}
-		return resourceGarageKeyCreate(ctx, d, m)
+		defer func() {
+			if resp.Body != nil {
+				_ = resp.Body.Close()
+			}
+		}()
 	}
-	return nil
+
+	return resourceGarageKeyRead(ctx, d, m)
 }
 
 func resourceGarageKeyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	// Note: Garage API doesn't have a delete key endpoint in v1
-	// We'll just remove from state
+	client := m.(*GarageClient)
+	keyID := d.Id()
+
+	resp, err := client.Client.AccessKeyAPI.DeleteKey(client.WithAuth(ctx)).Id(keyID).Execute()
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("failed to delete key: %w", err))
+	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
+
 	d.SetId("")
 	return nil
 }
